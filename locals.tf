44dd07601c82b948d9b822cda5ba9bc4c90d50d2
Mitul Shah <mitulshah@microsoft.com>
2024-07-14 03:23:10 -0500

    Moved config values to vars

--- locals.tf
+++ locals.tf
@@ -1,14 +1,14 @@
 locals {
-  resource_suffix           = [lower(var.environment), lower(var.region), substr(lower(var.application), 0, 3), substr(lower(var.owner), 0, 3), var.resource_group_name_suffix]
+  resource_suffix           = [lower(var.environment), lower(var.region), substr(lower(var.application), 0, 3), substr(lower(replace(var.owner_name, " ", "")), 0, 5), var.resource_group_name_suffix]
   resource_suffix_kebabcase = join("-", local.resource_suffix)
 
-  chat_model_name               = "gpt-35-turbo"
+  apim_sku = join("_", [var.apim_sku, var.apim_units])
 
   tags = merge(
     var.tags,
     tomap(
       {
-        "Owner"       = var.owner,
+        "Owner"       = var.owner_name,
         "Environment" = var.environment,
         "Region"      = var.region,
         "Application" = var.application
--- variables.tf
+++ variables.tf
@@ -26,15 +26,55 @@ variable "location" {
   default     = "eastus"
 }
 
-variable "owner" {
+variable "owner_name" {
   description = "The name of the project's owner"
   type        = string
-  default     = "mshah"
+  default     = "owner"
+}
+
+variable "owner_email" {
+  description = "The email of the project's owner"
+  type        = string
+  default     = "email@domain.com"
+}
+
+variable "apim_sku" {
+  description = "The SKU of the API Management instance"
+  type        = string
+  default     = "Developer"
+  validation {
+    condition     = can(regex("(Consumption|Developer|Basic|Standard|Premium)", var.apim_sku))
+    error_message = "The APIM SKU value must be a valid."
+  }
+}
+
+variable "apim_units" {
+  description = "The number of units for the API Management instance"
+  type        = number
+  default     = 1
+}
+
+variable "open_ai_model" {
+  description = "The name of the OpenAI model"
+  type        = string
+  default     = "gpt-35-turbo"
+}
+
+variable "open_ai_model_version" {
+  description = "The version of the OpenAI model"
+  type        = string
+  default     = "0301"
+}
+
+variable "open_ai_capacity" {
+  description = "The number of instances of the OpenAI model in thousands"
+  type        = number
+  default     = 5
 }
 
 variable "resource_group_name_suffix" {
   type        = string
-  default     = "001"
+  default     = "01"
   description = "The resource group name suffix"
   validation {
     condition     = can(regex("[0-9]{2}", var.resource_group_name_suffix))
